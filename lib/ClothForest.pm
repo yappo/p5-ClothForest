41c5de828571972140af0a6afda029f1da44488e
Kazuhiro Osawa <yappo@shibuya.pl>
2011-11-15 18:05:21 +0900

    initial commit

---
+++ lib/ClothForest.pm
@@ -0,0 +1,32 @@
+package ClothForest;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+ClothForest -
+
+=head1 SYNOPSIS
+
+    use ClothForest;
+
+=head1 DESCRIPTION
+
+ClothForest is
+
+=head1 AUTHOR
+
+Kazuhiro Osawa E<lt>yappo {at} shibuya {dot} plE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
